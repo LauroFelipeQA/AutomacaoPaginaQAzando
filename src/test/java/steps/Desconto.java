2032be9b9481f84e69e42161853afcd9450e99e4
Lauro <laurofelipeqa@gmail.com>
2023-05-04 11:14:51 -0300

    Projeto v2 -Melhoria na classe RunCucumberTest, usando BeforeClass e AfterClass para inicializar e finalizar o webdriver fora dos steps. README atualizado

--- src/test/java/steps/Desconto.java
+++ src/test/java/steps/Desconto.java
@@ -6,11 +6,10 @@ import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
 import org.junit.Assert;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Desconto {
+import runner.RunCucumberTest;
 
-    WebDriver driver = new ChromeDriver();
+public class Desconto extends RunCucumberTest {
 
     @Dado("^que estou no site da qazando$")
     public void que_estou_no_site_da_qazando(){
@@ -35,8 +34,6 @@ public class Desconto {
     public void eu_vejo_o_codigo_de_desconto() {
         String texto_cupom = driver.findElement(By.cssSelector("#cupom > h2 > span")).getText();
         Assert.assertEquals("O cumpom esta errado","QAZANDO15OFF",texto_cupom);
-
-        driver.quit();
     }
 
 }
